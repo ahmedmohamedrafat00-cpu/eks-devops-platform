92fd03c529956af36d44e1392a256a4a27e12def
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2026-01-13 12:28:40 +0200

    add ansible-sg

--- terraform/network/outputs.tf
+++ terraform/network/outputs.tf
@@ -9,3 +9,8 @@ output "public_subnets" {
 output "private_subnets" {
   value = aws_subnet.private[*].id
 }
+
+output "ansible_security_group_id" {
+  description = "Security group ID for Ansible control machine"
+  value       = aws_security_group.ansible_sg.id
+}
---
+++ terraform/network/security-groups.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "ansible_sg" {
+  name        = "ansible-sg"
+  description = "Security group for Ansible control machine"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    description = "SSH from Bastion"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  ingress {
+    description = "EKS API access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ansible-sg"
+  }
+}
