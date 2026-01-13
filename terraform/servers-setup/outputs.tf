c08e3da70560f233b5a5ae0e72dbc49cff1a89bd
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2026-01-13 12:44:26 +0200

    expose ansible private ip in outputs

--- terraform/servers-setup/outputs.tf
+++ terraform/servers-setup/outputs.tf
@@ -5,3 +5,8 @@ output "jenkins_public_ip" {
 output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
+
+output "ansible_private_ip" {
+  description = "Private IP of Ansible control machine"
+  value       = aws_instance.ansible.private_ip
+}
