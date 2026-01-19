a3b6e7dd322490b1f572e7a381e1b44fa3e7ba04
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2026-01-19 22:53:29 +0200

    Add Ansible structure with dynamic inventory via bastion

--- ansible/scripts/render-inventory.sh
+++ ansible/scripts/render-inventory.sh
@@ -1,9 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
-BASTION_IP=$(terraform -chdir=../terraform/servers-setup output -raw bastion_public_ip)
-ANSIBLE_PRIVATE_IP=$(terraform -chdir=../terraform/servers-setup output -raw ansible_private_ip)
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
-sed \
-  -e "s|\${BASTION_IP}|$BASTION_IP|g" \
-  -e "s|\${ANSIBLE_PRIVATE_IP}|$ANSIBLE_PRIVATE_IP|g" \
-  inventory/hosts.ini > inventory/hosts.generated.ini
+TF_DIR="$ROOT_DIR/../terraform/servers-setup"
+OUT_FILE="$ROOT_DIR/inventory/hosts.generated.ini"
+
+BASTION_IP=$(terraform -chdir="$TF_DIR" output -raw bastion_public_ip)
+ANSIBLE_IP=$(terraform -chdir="$TF_DIR" output -raw ansible_private_ip)
+
+cat > "$OUT_FILE" <<EOF
+[bastion]
+bastion ansible_host=${BASTION_IP}
+
+[ansible_servers]
+ansible ansible_host=${ANSIBLE_IP}
+
+[all:vars]
+ansible_user=ec2-user
+ansible_ssh_private_key_file=~/.ssh/eks-devops-key
+ansible_become=true
+ansible_become_method=sudo
+ansible_ssh_common_args='-o ForwardAgent=yes -o ProxyJump=ec2-user@${BASTION_IP} -o StrictHostKeyChecking=no'
+EOF
+
+echo "Inventory generated at $OUT_FILE"
